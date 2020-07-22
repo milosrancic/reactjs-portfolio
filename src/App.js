a2a2acbcab390637a8a92c383331210372ab1e46
Mita <milosrancic94@gmail.com>
2020-07-22 18:20:09 +0200

    //

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Blog from './Blog';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <ul>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+        </Switch>
+      </Router>
       home
       about 
       work
       contact
-      blog
-
-      
     </div>
   );
 }
