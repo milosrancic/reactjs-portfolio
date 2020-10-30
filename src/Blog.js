ed40714a1c3ec4bf7d1b690fdac4a99a66eeac23
Mita <milosrancic94@gmail.com>
2020-10-30 19:33:31 +0100

    //

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Blog = () => (
-    <h3>Blog is coming soon, I'm working on it &#1F642</h3>
+    <h3>Blog is coming soon, I'm working on it!</h3>
 );
 
 export default Blog;
