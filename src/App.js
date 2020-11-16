6533cc19e158d0525866cf0719208699bc630739
Mita <milosrancic94@gmail.com>
2020-11-16 21:46:39 +0100

    added nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 import Blog from "./Blog";
 
@@ -20,7 +21,22 @@ function App() {
           </Route>
         </Switch>
       </Router> */}
-      <div className="navbar">navigation</div>
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home" className="">
+            Home?
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about-me">About me</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/projects">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/blog">Blog</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
