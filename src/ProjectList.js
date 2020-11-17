8fdfd73434ee5912df490b0f4b287f3f37d07366
Mita <milosrancic94@gmail.com>
2020-11-17 22:29:29 +0100

    dd

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -4,43 +4,43 @@ import Project from "./Project";
 const ProjectList = (props) => (
   <>
     <Project
-      title="Restaurant"
+      title="restaurant"
       tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
       live="https://milosrancic.github.io/restaurant-website/"
       github="https://github.com/milosrancic/restaurant-website"
     />
 
     <Project
-      title="Charity"
+      title="charity"
       tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
       live="https://milosrancic.github.io/charity-website/"
       github="https://github.com/milosrancic/charity-website"
     />
 
     <Project
-      title="Beverage"
-      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      title="beverage"
+      tech="HTML, CSS, JavaScript, Bootstrap"
       live="https://milosrancic.github.io/beverage-website/"
       github="https://github.com/milosrancic/beverage-website"
     />
 
     <Project
       title="bookstore"
-      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      tech="HTML, CSS, Sass/SCSS, Bootstrap, JavaScript, ReactJS"
       live="https://milosrancic.github.io/reactjs-bookstore/"
       github="https://github.com/milosrancic/reactjs-bookstore"
     />
 
     <Project
       title="weather app"
-      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      tech="HTML, CSS, JavaScript, ReactJS"
       live="https://milosrancic.github.io/reactjs-weather-app/"
       github="https://github.com/milosrancic/reactjs-weather-app"
     />
 
     <Project
       title="quote machine"
-      tech="HTML5, CSS3, JavaScript, ReactJS"
+      tech="HTML, CSS, JavaScript, ReactJS"
       live="https://milosrancic.github.io/reactjs-quote-machine"
       github="https://github.com/milosrancic/reactjs-quote-machine"
     />
