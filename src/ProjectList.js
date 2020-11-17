c807a3d43a9773bd91168640f8a13a12b6fdf938
Mita <milosrancic94@gmail.com>
2020-11-17 22:27:29 +0100

    created projectlist component

---
+++ src/ProjectList.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Project from "./Project";
+
+const ProjectList = (props) => (
+  <>
+    <Project
+      title="Restaurant"
+      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      live="https://milosrancic.github.io/restaurant-website/"
+      github="https://github.com/milosrancic/restaurant-website"
+    />
+
+    <Project
+      title="Charity"
+      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      live="https://milosrancic.github.io/charity-website/"
+      github="https://github.com/milosrancic/charity-website"
+    />
+
+    <Project
+      title="Beverage"
+      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      live="https://milosrancic.github.io/beverage-website/"
+      github="https://github.com/milosrancic/beverage-website"
+    />
+
+    <Project
+      title="bookstore"
+      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      live="https://milosrancic.github.io/reactjs-bookstore/"
+      github="https://github.com/milosrancic/reactjs-bookstore"
+    />
+
+    <Project
+      title="weather app"
+      tech="HTML, CSS, JavaScript, ReactJS, Bootstrap"
+      live="https://milosrancic.github.io/reactjs-weather-app/"
+      github="https://github.com/milosrancic/reactjs-weather-app"
+    />
+
+    <Project
+      title="quote machine"
+      tech="HTML5, CSS3, JavaScript, ReactJS"
+      live="https://milosrancic.github.io/reactjs-quote-machine"
+      github="https://github.com/milosrancic/reactjs-quote-machine"
+    />
+  </>
+);
+
+export default ProjectList;
